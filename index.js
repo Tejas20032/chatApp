c6d1469781e4281b4bc27d168488b02ecf24be2f
Tejas HK <115473875+Tejas20032@users.noreply.github.com>
2024-09-07 19:43:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
-const http=require("http");
-const express=require("express");
-const cors=require("cors");
-const socketIO=require("socket.io");
-const { disconnect } = require("process");
+const express = require('express');
+const cors = require('cors');
+const socketIO = require('socket.io');
+const http = require('http');
 
-
-const app=express();
-const port=process.env.PORT;
-
-const users=[{}];
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-})
-const server=http.createServer(app);
 
-const io=socketIO(server);
-
-io.on("connection",(socket)=>{
-    console.log("New connection");
-
-    socket.on('joined',({user})=>{
-        users[socket.id]=user;
-        console.log(`${user} has joined`);
-        socket.broadcast.emit('userJoined',{user:"Admin",message:`${users[socket.id]} has joined`})
-        socket.emit('welcome',{user:"Admin",message:`Welcome to the chat,${users[socket.id]}`})
-
-    })
-    socket.on('disconnect',()=>{
-        socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]} has left`}) ;
-        console.log("User left");
-    })
-  
-    socket.on('message',({message,id})=>{
-        io.emit('sendMessage',{user:users[id],message,id});
-
-    })
-   
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-server.listen(port,() =>{
-    console.log(`Server is working on http://localhost:${port}`);
+const server = http.createServer(app);
+const io = socketIO(server);
 
-})
+io.on("connection", (socket) => {
+  console.log("New connection");
+
+  socket.on('joined', ({ user }) => {
+    users[socket.id] = user;
+    console.log(`${user} has joined`);
+    socket.broadcast.emit('userJoined', { user: "Admin", message: `${users[socket.id]} has joined` });
+    socket.emit('welcome', { user: "Admin", message: `Welcome to the chat, ${users[socket.id]}` });
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('leave', { user: "Admin", message: `${users[socket.id]} has left` });
+    console.log("User left");
+  });
+
+  socket.on('message', ({ message, id }) => {
+    io.emit('sendMessage', { user: users[id], message, id });
+  });
+});
+
+module.exports = (req, res) => {
+  return new Promise((resolve) => {
+    server.listen(port, () => {
+      console.log(`Server is working on http://localhost:${port}`);
+      resolve();
+    });
+  });
+};
